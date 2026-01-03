b8a4e31e8aee92baf62c961e172b9664781e2d48
rainanirmal <rainanirmal01@gmail.com>
2026-01-03 23:33:50 +0530

    Add pattern9 solution

---
+++ Basics/Patterns/pattern9.java
@@ -0,0 +1,20 @@
+package Basics.Patterns;
+
+public class pattern9 {
+    
+    public static void main(String[] args) {
+        
+        for(int i = 1 ; i <= 5 ; i ++) {
+            for(int j = 1 ; j <= i ; j ++) {
+                System.out.print((i + j) % 2 + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// 0 
+// 1 0
+// 0 1 0
+// 1 0 1 0
+// 0 1 0 1 0
