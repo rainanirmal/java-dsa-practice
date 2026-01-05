f47aee9ed92c1e47859bafae17c7b6c2e101ed2a
rainanirmal <rainanirmal01@gmail.com>
2026-01-05 14:50:47 +0530

    Add pattern11 solution

---
+++ Basics/Patterns/pattern11.java
@@ -0,0 +1,24 @@
+package Basics.Patterns;
+
+public class pattern11 {
+    
+    public static void main(String[] args) {
+        
+        for(int i = 4 ; i > 0 ; i --) {
+            for(int j = 1 ; j <= i ; j ++) {
+                System.out.print("  ");
+            }
+            for(int j = 1 ; j <= 5 - i ; j ++) {
+                System.out.print("* ");
+            }
+            for(int j = 1 ; j <= 4 - i ; j ++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+//         * 
+//       * * *
+//     * * * * *
+//   * * * * * * *
