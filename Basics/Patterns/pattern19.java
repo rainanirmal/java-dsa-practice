87acbc182befa6e725418eaeec0a224392116ce2
rainanirmal <rainanirmal01@gmail.com>
2026-01-11 12:31:14 +0530

    Add comments in pattern19

--- Basics/Patterns/pattern19.java
+++ Basics/Patterns/pattern19.java
@@ -5,7 +5,6 @@ public class pattern19 {
     public static void main(String[] args) {
         
         for(int i = 1 ; i <= 5 ; i ++) {
-
             char ch = (char)('E' - i + 1);
             for(int j = 1 ; j <= i ; j ++) {
                 System.out.print(ch + " ");
@@ -20,3 +19,10 @@ public class pattern19 {
 // C D E
 // B C D E
 // A B C D E
+
+
+// 69 - 1 + 1	69	E
+// 69 - 2 + 1	68	D
+// 69 - 3 + 1	67	C
+// 69 - 4 + 1	66	B
+// 69 - 5 + 1	65	A
