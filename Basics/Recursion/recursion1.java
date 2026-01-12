b6a6a25b826d37a531d2c1f77e4fc96fda076350
rainanirmal <rainanirmal01@gmail.com>
2026-01-12 11:47:46 +0530

    Create new recursion folder and add recursion1 solution

---
+++ Basics/Recursion/recursion1.java
@@ -0,0 +1,32 @@
+package Basics.Recursion;
+
+import java.util.*;
+
+public class recursion1 {
+    
+    public static void print_name(String name , int count , int n) {
+        
+        if(count == n + 1) {
+            return;
+        }
+
+        System.out.println("Name : " +name+ " , Count : " +count);
+
+        print_name(name, count + 1, n);
+    }
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter name : ");
+        String name = sc.next();
+
+        System.out.println("Enter number : ");
+        int n = sc.nextInt();
+
+        print_name(name, 1, n);
+
+        sc.close();
+    }
+}
