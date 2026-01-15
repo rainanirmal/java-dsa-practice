9e15c27c95ca4e0cf15b894791984a2c4c056eb4
rainanirmal <rainanirmal01@gmail.com>
2026-01-15 23:17:04 +0530

    Created bubble sort

---
+++ Sorting/bubble_sort.java
@@ -0,0 +1,23 @@
+package Sorting;
+
+import java.util.*;
+
+public class bubble_sort {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter size of array : ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements of array : ");
+        for(int i = 0 ; i < n ; i ++) {
+            arr[i] = sc.nextInt();
+        }
+
+        sc.close();
+    }
+}
