37ab4f0a947406696f394e62becb0a5efcf12714
rainanirmal <rainanirmal01@gmail.com>
2026-01-16 19:02:37 +0530

    Add bubble sort solution

--- Sorting/bubble_sort.java
+++ Sorting/bubble_sort.java
@@ -4,6 +4,18 @@ import java.util.*;
 
 public class bubble_sort {
     
+    public static void bubble(int[] arr , int n) {
+
+        for(int i = n - 1 ; i >= 1 ; i --) {
+            for(int j = 0 ; j <= i - 1; j ++) {
+                if(arr[j] > arr[j + 1]) {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    } 
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
@@ -16,6 +28,13 @@ public class bubble_sort {
         System.out.println("Enter elements of array : ");
         for(int i = 0 ; i < n ; i ++) {
             arr[i] = sc.nextInt();
+        }
+
+        bubble(arr, n);
+
+        System.out.println("Sorted array : ");
+        for(int i = 0 ; i < n ; i ++) {
+            System.out.print(arr[i] + " ");
         }
 
         sc.close();
