ce71277a4ea71c9e8967078417d547642646af28
rainanirmal <rainanirmal01@gmail.com>
2026-01-27 02:05:38 +0530

    Add longest consecutive sequence solution

--- Array_medium/longest_consecutive_sequence.java
+++ Array_medium/longest_consecutive_sequence.java
@@ -7,7 +7,37 @@ import java.util.*;
 
 class longest_consecutive_sequence {
 
-    public static void main(String[] args) {
+    public static void consecutive(int[] arr , int n ) {
+
+        for(int i = n - 1 ; i > 0 ; i --) {
+            for(int j = 0 ; j < i ; j ++) {
+                if(arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        int count = 1;
+        int max = 1;
+
+        for(int i = 0 ; i < n - 1 ; i ++) {
+            if(arr[i + 1] == arr[i] + 1) {
+                count++;
+            }
+            else {
+                count = 1;
+            }
+
+            if(count > max) {
+                max = count;
+            }
+        }
+
+        System.out.println("Longest consecutive array length : " +max);
+    }
+     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
 
@@ -20,6 +50,8 @@ class longest_consecutive_sequence {
         for(int i = 0 ; i < n ; i ++) {
             arr[i] = sc.nextInt();
         }
+
+        consecutive(arr, n);
 
         sc.close();
     }
