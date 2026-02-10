f1ba53eba86d0acb59e900aa3686595538d2c3a0
rainanirmal <rainanirmal01@gmail.com>
2026-02-10 23:42:09 +0530

    Created search in rotated array

---
+++ Binary_search_on_1D_array/search_in_rotated_array.java
@@ -0,0 +1,24 @@
+package Binary_search_on_1D_array;
+
+import java.util.*;
+
+public class search_in_rotated_array {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter size of array : ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements of array : ");
+        for(int i = 0 ; i < n ; i ++) {
+            arr[i] = sc.nextInt();
+        }
+
+        sc.close();
+    }
+    
+}
