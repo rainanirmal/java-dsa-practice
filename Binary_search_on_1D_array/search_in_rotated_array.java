c6c0aadda58d224fe592e30a29efeebc47d7f3eb
rainanirmal <rainanirmal01@gmail.com>
2026-02-12 16:29:14 +0530

    Add search in rotated array solution

--- Binary_search_on_1D_array/search_in_rotated_array.java
+++ Binary_search_on_1D_array/search_in_rotated_array.java
@@ -4,6 +4,29 @@ import java.util.*;
 
 public class search_in_rotated_array {
 
+    public static int search_in_rotated(int[] arr , int n , int k) {
+
+        int low = 0;
+        int high = n - 1;
+
+        while (low <= high) {
+            
+            int mid = low + (high - low) / 2;
+
+            if(arr[mid] == k) {
+                return mid;
+            }
+
+            if((arr[low] <= k) && (k < arr[mid])) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
@@ -18,7 +41,42 @@ public class search_in_rotated_array {
             arr[i] = sc.nextInt();
         }
 
+        System.out.println("Enter target : ");
+        int k = sc.nextInt();
+
+        int result = search_in_rotated(arr, n, k);
+
+        System.out.println("Target index : " + result);
+
         sc.close();
     }
-    
 }
+
+// output
+// Enter size of array : 
+// 7
+// Enter elements of array : 
+// 4
+// 5
+// 6
+// 7
+// 0
+// 1
+// 2
+// Enter target : 
+// 0
+// Target index : 4
+
+// Enter size of array : 
+// 7
+// Enter elements of array : 
+// 4
+// 5
+// 6
+// 7
+// 0
+// 1
+// 2
+// Enter target : 
+// 3
+// Target index : -1
