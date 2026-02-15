cab1d61a6d8ff9527ceff222ee02b51bb6f2ef20
rainanirmal <rainanirmal01@gmail.com>
2026-02-15 23:50:58 +0530

    Created nth root number

---
+++ Binary_search_on_answer/nth_root_of_number.java
@@ -0,0 +1,28 @@
+package Binary_search_on_answer;
+
+import java.util.*;
+
+public class nth_root_of_number {
+
+    public static void root(int n , int m) {
+
+        
+        
+    }
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter nth root of a number : ");
+        int n = sc.nextInt();
+
+        System.out.println("Enter m number : ");
+        int m = sc.nextInt();
+
+        
+
+        sc.close();
+    }
+    
+}
