8c111db416707e906b04fb648fad49fd91cd410d
rainanirmal <rainanirmal01@gmail.com>
2026-02-17 18:09:15 +0530

    Add nth root of number solution

--- Binary_search_on_answer/nth_root_of_number.java
+++ Binary_search_on_answer/nth_root_of_number.java
@@ -4,10 +4,41 @@ import java.util.*;
 
 public class nth_root_of_number {
 
-    public static void root(int n , int m) {
+    public static int power(int mid , int n) {
 
-        
-        
+        int ans = 1;
+
+        for(int i = 1 ; i <= n ; i ++) {
+            ans = ans * mid;
+        }
+
+        return ans;
+    }
+
+    public static int root(int n , int m) {
+
+        int low = 1;
+        int high = m;
+
+        while (low <= high) {
+            
+            int mid = low + (high - low) / 2;
+
+            int power = power(mid, n);
+
+            if(power == m) {
+                return mid;
+            }
+
+            if(power < m) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
     }
 
     public static void main(String[] args) {
@@ -20,7 +51,29 @@ public class nth_root_of_number {
         System.out.println("Enter m number : ");
         int m = sc.nextInt();
 
+        int result = root(n, m);
+        
+        if(result == -1) {
+            System.out.println("The " + n + " root of " + m + " does not exists.");
+        }
+        else {
+            System.out.println("The " + n + " root of " + m + " is " + result + "(" + result + "^" + n + " = " + m +")");
+        }
+
         sc.close();
     }
     
 }
+
+// output
+// Enter nth root of a number : 
+// 4
+// Enter m number : 
+// 16
+// The 4 root of 16 is 2(2^4 = 16)
+
+// Enter nth root of a number : 
+// 3
+// Enter m number : 
+// 27
+// The 3 root of 27 is 3(3^3 = 27)
