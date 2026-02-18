d4202cdce8999e9905ee5dec1c7c3387a8254412
rainanirmal <rainanirmal01@gmail.com>
2026-02-18 23:59:41 +0530

    Create koko eating banana

---
+++ Binary_search_on_answer/koko_eating_bananas.java
@@ -0,0 +1,17 @@
+// A monkey is given n piles of bananas, where the 'ith' pile has nums[i] bananas. An integer h represents the total time in hours to eat all the bananas.
+// Each hour, the monkey chooses a non-empty pile of bananas and eats k bananas. If the pile contains fewer than k bananas, the monkey eats all the bananas in that pile and does not consume any more bananas in that hour.
+// Determine the minimum number of bananas the monkey must eat per hour to finish all the bananas within h hours.
+
+package Binary_search_on_answer;
+
+import java.util.*;
+
+public class koko_eating_bananas {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+    }
+}
