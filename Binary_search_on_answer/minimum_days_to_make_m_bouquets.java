ec9e871efa634f64758faffa9d8315154d48ce4f
rainanirmal <rainanirmal01@gmail.com>
2026-02-20 23:35:49 +0530

    Careated for bouquets

---
+++ Binary_search_on_answer/minimum_days_to_make_m_bouquets.java
@@ -0,0 +1,10 @@
+// Given n roses and an array nums where nums[i] denotes that the 'ith' rose will bloom on the nums[i]th day, only adjacent bloomed roses can be picked to make a bouquet. Exactly k adjacent bloomed roses are required to make a single bouquet. Find the minimum number of days required to make at least m bouquets, each containing k roses. Return -1 if it is not possible.
+
+package Binary_search_on_answer;
+
+import java.util.*;
+
+public class minimum_days_to_make_m_bouquets {
+
+    
+}
