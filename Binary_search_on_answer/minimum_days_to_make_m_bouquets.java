f4d132017bd3ffd92605ec33b9bb43721a652d17
rainanirmal <rainanirmal01@gmail.com>
2026-02-26 23:55:32 +0530

    Add minimum days to make m bouquets solution

--- Binary_search_on_answer/minimum_days_to_make_m_bouquets.java
+++ Binary_search_on_answer/minimum_days_to_make_m_bouquets.java
@@ -6,6 +6,81 @@ import java.util.*;
 
 public class minimum_days_to_make_m_bouquets {
 
+    public static int low(int[] arr) {
+        int low = arr[0];
+
+        for(int i = 0 ; i < arr.length ; i ++) {
+            if (arr[i] < low) {
+                low = arr[i];
+            }
+        }
+
+        return low;
+    }
+
+    public static int high(int[] arr) {
+
+        int high = arr[0];
+        for(int i = 1 ; i < arr.length ; i ++) {
+            if(arr[i] > high) {
+                high = arr[i];
+            }
+        }
+
+        return high;
+    }
+
+    public static boolean days(int[] arr , int mid , int m , int k) {
+
+        int flower = 0;
+        int bouquets = 0;
+
+        for(int i = 0 ; i < arr.length ; i ++) {
+
+            if (arr[i] <= mid) {
+                flower++;
+
+                if (flower == k) {
+                    bouquets++;
+                    flower = 0;
+                }
+            }
+            else {
+                flower = 0;
+            }
+        }
+
+        if (bouquets >= m) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public static int min_days(int[] arr , int n , int m , int k) {
+
+        int low = low(arr);
+        int high = high(arr);
+
+        int ans = -1;
+
+        while (low <= high) {
+            
+            int mid = low + (high - low) / 2;
+
+            if (days(arr, mid, m, k)) {
+                ans = mid;
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
@@ -26,6 +101,42 @@ public class minimum_days_to_make_m_bouquets {
         System.out.println("Enter number of bouquets : ");
         int m = sc.nextInt();
 
+        int result = min_days(arr, n, m, k);
+
+        System.out.println("Minimum days to make " + m + " bouquets : " + result);
+
         sc.close();
     }    
 }
+
+// output
+// Enter number of roses : 
+// 8
+// Enter days of bloom : 
+// 7
+// 7
+// 7
+// 7
+// 13
+// 11
+// 12
+// 7
+// Enter number of required bloomed roses :
+// 3
+// Enter number of bouquets :
+// 2
+// Minimum days to make 2 bouquets : 12
+
+// Enter number of roses : 
+// 5
+// Enter days of bloom : 
+// 1
+// 10
+// 3
+// 10
+// 2
+// Enter number of required bloomed roses : 
+// 2
+// Enter number of bouquets : 
+// 3
+// Minimum days to make 3 bouquets : -1
