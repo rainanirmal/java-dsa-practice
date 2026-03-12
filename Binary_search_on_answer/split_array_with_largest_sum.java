d2ce9f25670fb7507c38bb713744d5555234a442
rainanirmal <rainanirmal01@gmail.com>
2026-03-13 00:43:41 +0530

    Add split array with largest sum solution

--- Binary_search_on_answer/split_array_with_largest_sum.java
+++ Binary_search_on_answer/split_array_with_largest_sum.java
@@ -6,6 +6,73 @@ import java.util.*;
 
 public class split_array_with_largest_sum {
 
+    public static int max (int[] arr) {
+
+        int max = arr[0];
+
+        for(int i = 0 ; i < arr.length ; i ++) {
+            if(arr[i] > max) {
+                max = arr[i];
+            } 
+        }
+
+        return max;
+    }
+
+    public static int sum(int[] arr) {
+
+        int sum = 0;
+
+        for(int i = 0 ; i < arr.length ; i ++) {
+            sum = sum + arr[i];
+        }
+
+        return sum;
+    }
+
+    public static int split(int[] arr , int mid , int k) {
+
+        int recent = 0;
+        int count = 1;
+
+        for(int i = 0 ; i < arr.length ; i ++) {
+            if(recent + arr[i] > mid) {
+                count++;;
+                recent = arr[i];
+            }
+            else {
+                recent = recent + arr[i];
+            }
+        }
+
+        return count;
+    }
+
+    public static int sub_array(int[] arr , int n , int k) {
+        
+        int low = max(arr);
+        int high = sum(arr);
+
+        int result = high;
+        
+        while (low <= high) {
+            
+            int mid = low + (high - low) / 2;
+
+            int ans = split(arr, mid, k);
+
+            if(ans <= k ) {
+                result = mid;
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
@@ -23,6 +90,33 @@ public class split_array_with_largest_sum {
         System.out.println("Enter number of subarrays to be created : ");
         int k = sc.nextInt();
 
+        int result = sub_array(arr, n, k);
+
+        System.out.println("Number of subarray : " +result);
+
         sc.close();
     }
 }
+
+// output
+// Enter size of array : 
+// 5
+// Enter elements of array : 
+// 1
+// 2
+// 3
+// 4
+// 5
+// Enter number of subarrays to be created : 
+// 3
+// Number of subarray : 6
+
+// Enter size of array : 
+// 3
+// Enter elements of array : 
+// 3
+// 5
+// 1
+// Enter number of subarrays to be created : 
+// 3
+// Number of subarray : 5
