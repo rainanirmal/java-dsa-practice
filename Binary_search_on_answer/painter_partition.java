fd7bc0ee894dec08d05fcbc2564401a9798197e4
rainanirmal <rainanirmal01@gmail.com>
2026-03-13 23:54:44 +0530

    Add painter's partition main method

---
+++ Binary_search_on_answer/painter_partition.java
@@ -0,0 +1,21 @@
+// You are given A painters and an array C of N integers where C[i] denotes the length of the ith board. Each painter takes B units of time to paint 1 unit of board. You must assign boards to painters such that:
+
+// Each painter paints only contiguous segments of boards.
+// No board can be split between painters.
+// The goal is to minimize the time to paint all boards
+
+package Binary_search_on_answer;
+
+import java.util.*;
+
+public class painter_partition {
+    
+    public static void main(String[] args) {
+    
+        Scanner sc = new Scanner(System.in);
+
+
+
+        sc.close();
+    }
+}
