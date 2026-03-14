8aa81df7a792f207f2daf400d9166a41281339f7
rainanirmal <rainanirmal01@gmail.com>
2026-03-14 23:44:39 +0530

    Changed main method of painter's partition solution

--- Binary_search_on_answer/painter_partition.java
+++ Binary_search_on_answer/painter_partition.java
@@ -9,12 +9,27 @@ package Binary_search_on_answer;
 import java.util.*;
 
 public class painter_partition {
+
+    public static void time_to_paint_all_boards(int[] arr , int n , int a , int b) {}
     
     public static void main(String[] args) {
     
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Enter number of painters : ");
+        int a = sc.nextInt();
 
+        System.out.println("Enter number of boards : ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        System.out.println("Enter length of each board : ");
+        for(int i = 0 ; i < n ; i ++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter time of painter take to paint 1 unit of board : ");
+        int b = sc.nextInt();
 
         sc.close();
     }
