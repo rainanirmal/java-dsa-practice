bea4d8d80cbf2537de939288b29e65bcd3743f49
rainanirmal <rainanirmal01@gmail.com>
2026-04-23 23:43:40 +0530

    refactor : improved bubble sorting code structure

--- Sorting/bubble_sort.java
+++ Sorting/bubble_sort.java
@@ -6,16 +6,36 @@ public class bubble_sort {
     
     public static void bubble(int[] arr , int n) {
 
-        for(int i = n - 1 ; i >= 1 ; i --) {
-            for(int j = 0 ; j <= i - 1; j ++) {
-                if(arr[j] > arr[j + 1]) {
-                    int temp = arr[j + 1];
-                    arr[j + 1] = arr[j];
-                    arr[j] = temp;
+        // for(int i = n - 1 ; i >= 1 ; i --) {
+        //     for(int j = 0 ; j <= i - 1; j ++) {
+        //         if(arr[j] > arr[j + 1]) {
+        //             int temp = arr[j + 1];
+        //             arr[j + 1] = arr[j];
+        //             arr[j] = temp;
+        //         }
+        //     }
+        // }
+
+
+        for(int i = 1 ; i < n ; i ++) {
+            int didSwap = 0;
+            for(int j = 0 ; j < n - i - 1 ; j ++) {
+                if (arr[j] > arr[j + 1]) {
+                    bubble_swap(arr, j);
+                    didSwap = 1;
                 }
+            }
+            if (didSwap == 0) {
+                break; //used to break the loop if elements are already in sorted order
             }
         }
     } 
+
+    public static void bubble_swap(int[] arr , int j) {
+        int temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+    }
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
