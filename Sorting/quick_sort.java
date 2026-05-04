7cda1d4706a769a0757651ef6b170318303f25a1
rainanirmal <rainanirmal01@gmail.com>
2026-05-04 23:34:48 +0530

    dsa : add quick sort main method

---
+++ Sorting/quick_sort.java
@@ -0,0 +1,24 @@
+package Sorting;
+
+import java.util.*;
+
+public class quick_sort {
+
+    public static void quick() {}
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter size of array : ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements of array : ");
+        for(int i = 0 ; i < n ; i ++) {
+            arr[i] = sc.nextInt();
+        }
+    }
+    
+}
