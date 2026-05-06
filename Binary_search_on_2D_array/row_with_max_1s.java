7b32c8c8a4aaf90fcdf16bcae39ea21552b4c000
rainanirmal <rainanirmal01@gmail.com>
2026-05-06 22:16:48 +0530

    dsa : add row with maximum 1's in matrix main method

---
+++ Binary_search_on_2D_array/row_with_max_1s.java
@@ -0,0 +1,29 @@
+package Binary_search_on_2D_array;
+
+import java.util.*;
+
+public class row_with_max_1s {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number of rows : ");
+        int row = sc.nextInt();
+
+        System.out.println("Enter number of columns : ");
+        int col = sc.nextInt();
+
+        int[][] matrix = new int[row][col];
+
+        System.out.println("Enter elements of matrix : ");
+        for(int i = 0 ; i < row ; i ++) {
+            for(int j = 0 ; j < col ; j ++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        sc.close();
+    }
+    
+}
