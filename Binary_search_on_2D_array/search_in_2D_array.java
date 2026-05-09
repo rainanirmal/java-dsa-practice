5cc7176cd412b39b465e97ba87318012f82b473b
rainanirmal <rainanirmal01@gmail.com>
2026-05-09 23:43:11 +0530

    dsa : add main method of search target in 2D array

---
+++ Binary_search_on_2D_array/search_in_2D_array.java
@@ -0,0 +1,33 @@
+// Given a 2-D array mat where the elements of each row are sorted in non-decreasing order, and the first element of a row is greater than the last element of the previous row (if it exists), and an integer target, determine if the target exists in the given mat or not.
+
+package Binary_search_on_2D_array;
+
+import java.util.*;
+
+public class search_in_2D_array {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number of rows : ");
+        int row = sc.nextInt();
+
+        System.out.println("Enter number of columns : ");
+        int col = sc.nextInt();
+
+        int[][] matrix = new int[row][col];
+
+        System.out.println("Enter elements of matrix : ");
+        for(int i = 0 ; i < row ; i ++) {
+            for(int j = 0 ; j < col ; j ++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Enter target : ");
+        int target = sc.nextInt();
+
+        sc.close();
+    }
+}
