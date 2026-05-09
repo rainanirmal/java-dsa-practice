fe36c2420d0a955730ab7aae23df8360c7c87de3
rainanirmal <rainanirmal01@gmail.com>
2026-05-09 23:37:29 +0530

    dsa : solve row with maximum 1s in matrix using binary search

--- Binary_search_on_2D_array/row_with_max_1s.java
+++ Binary_search_on_2D_array/row_with_max_1s.java
@@ -1,8 +1,47 @@
+// Given a non-empty grid mat consisting of only 0s and 1s, where all the rows are sorted in ascending order, find the index of the row with the maximum number of ones.
+// If two rows have the same number of ones, consider the one with a smaller index. If no 1 exists in the matrix, return -1.
+
 package Binary_search_on_2D_array;
 
 import java.util.*;
 
 public class row_with_max_1s {
+
+    public static int lower(int[] arr , int col , int x) {
+        int low = 0;
+        int high = col - 1;
+        int ans = col;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] >= x) {
+                ans = mid;
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static int row_index(int[][] arr , int row , int col) {
+        int index = -1;
+        int max_count = 0;
+
+        for(int i = 0 ; i < row ; i ++) {
+            int count = col - lower(arr[i], col, 1);
+
+            if(count > max_count) {
+                max_count = count;
+                index = i;
+            }
+        }
+
+        return index;
+    }
 
     public static void main(String[] args) {
         
@@ -23,7 +62,39 @@ public class row_with_max_1s {
             }
         }
 
+        int max = row_index(matrix, row, col);
+
+        System.out.println("Row with maximum 1 index : " + max);
+
         sc.close();
     }
     
 }
+
+// output
+// Enter number of rows : 
+// 3
+// Enter number of columns : 
+// 3
+// Enter elements of matrix : 
+// 1
+// 1
+// 1
+// 0
+// 1
+// 1
+// 0
+// 0
+// 0
+// Row with maximum 1 index : 0
+
+// Enter number of rows : 
+// 2
+// Enter number of columns : 
+// 2
+// Enter elements of matrix : 
+// 0
+// 0
+// 0
+// 0
+// Row with maximum 1 index : -1
