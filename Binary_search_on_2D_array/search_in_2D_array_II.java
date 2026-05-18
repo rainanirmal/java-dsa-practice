63fb1717bad062d6eaffde932669526502c7d8e3
rainanirmal <rainanirmal01@gmail.com>
2026-05-18 23:52:14 +0530

    dsa : add search logic for search in 2D array II

--- Binary_search_on_2D_array/search_in_2D_array_II.java
+++ Binary_search_on_2D_array/search_in_2D_array_II.java
@@ -1,8 +1,27 @@
+// Given a 2D array matrix where each row is sorted in ascending order from left to right and each column is sorted in ascending order from top to bottom, write an efficient algorithm to search for a specific integer target in the matrix.
+
 package Binary_search_on_2D_array;
 
 import java.util.Scanner;
 
 public class search_in_2D_array_II {
+
+    public static boolean target_2D(int[] r , int target) {
+
+        return false;
+
+    }
+
+    public static boolean search_in_2D(int[][] arr , int r , int c , int target) {
+
+        for(int i = 0 ; i < r ; i ++) {
+            if (target >= arr[i][0] && target <= arr[i][c]) {
+                return target_2D(arr[r], target);
+            }
+        }
+        
+        return false;
+    }
     
     public static void main(String[] args) {
         
