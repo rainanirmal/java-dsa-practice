142cccf9c87b410cf1da9ef9e69c1626235d62da
rainanirmal <rainanirmal01@gmail.com>
2026-05-22 23:36:04 +0530

    dsa : add main method of matrix median

---
+++ Binary_search_on_2D_array/matrix_median.java
@@ -0,0 +1,30 @@
+// Given a 2D array matrix that is row-wise sorted. The task is to find the median of the given matrix.
+
+package Binary_search_on_2D_array;
+
+import java.util.Scanner;
+
+public class matrix_median {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number of rows : ");
+        int row = sc.nextInt();
+
+        System.out.println("Enter number of columns : ");
+        int col = sc.nextInt();
+
+        int[][] matrix = new int[row][col];
+
+        System.out.println("Enter elements of matrix : ");
+        for(int i = 0 ; i < row ; i ++) {
+            for(int j = 0 ; j < col ; j ++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        sc.close();
+    }
+}
