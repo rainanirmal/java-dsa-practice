75914e909023ee993d4690f8f79caddeb2f17873
rainanirmal <rainanirmal01@gmail.com>
2026-06-12 15:23:48 +0530

    feat(dsa) : solve matrix diagonal absolute difference

--- Array_hard/matrix_diagonal_difference.java
+++ Array_hard/matrix_diagonal_difference.java
@@ -6,6 +6,29 @@ import java.util.*;
 
 public class matrix_diagonal_difference {
 
+    public static int diagonal_difference(int[][] matrix , int row , int column) {
+
+        int left_diagonal = 0;
+        int right_diagonal = 0;
+        int ans = 0;
+
+        for(int i = 0 ; i < row ; i ++) {
+            for(int j = i ; j <= i ; j ++) {
+                left_diagonal = left_diagonal + matrix[i][j];
+            }
+        }
+
+        for(int i = 0 ; i < row ; i ++) {
+            for(int j = 2 - i ; j <= 2 - i ; j ++) {
+                right_diagonal = right_diagonal + matrix[i][j];
+            }
+        }
+
+        ans = Math.abs(left_diagonal - right_diagonal);
+
+        return ans;
+    }
+
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
@@ -24,6 +47,10 @@ public class matrix_diagonal_difference {
                 matrix[i][j] = sc.nextInt();
             }
         }
+
+        int difference = diagonal_difference(matrix, row, column);
+
+        System.out.println("Absolute difference between the sums of diagonals : " + difference);
 
         sc.close();
     }
