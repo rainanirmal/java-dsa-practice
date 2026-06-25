a2847392aad7c65dd4632069a467fc74745bf163
rainanirmal <rainanirmal01@gmail.com>
2026-06-26 00:21:28 +0530

    feat(dsa) : solve insertion of integer at head of linked list

--- LinkedList/insertion_at_head.java
+++ LinkedList/insertion_at_head.java
@@ -9,18 +9,38 @@ class Node {
     int data;
     Node next;
 
-    Node(int data1 , Node next1) {
-        this.data = data1;
-        this.next = next1;
+    Node(int data , Node next) {
+        this.data = data;
+        this.next = next;
     }
 
-    Node(int data1) {
-        this.data = data1;
+    Node(int data) {
+        this.data = data;
         this.next = null;
     }
 }
 
 public class insertion_at_head {
+
+    private static Node convertToLL(int[] arr) {
+
+        Node head = new Node(arr[0]);
+        Node mover = head;
+
+        for(int i = 1 ; i < arr.length ; i ++) {
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover = temp;
+        }
+
+        return head;
+    }
+
+    private static Node insert_at_head(Node head , int value) {
+        
+        Node temp = new Node(value , head);
+        return temp;
+    }
 
     public static void main(String[] args) {
         
@@ -38,6 +58,15 @@ public class insertion_at_head {
 
         System.out.println("Enter element to add at head of linked list : ");
         int x = sc.nextInt();
+
+        Node head = convertToLL(list);
+        Node temp = insert_at_head(head, x);
+
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.print("null");
 
         sc.close();
     }
