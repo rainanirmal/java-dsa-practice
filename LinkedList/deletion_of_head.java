41dd7bd8d33311673e012c33ba417d3a68c50198
rainanirmal <rainanirmal01@gmail.com>
2026-06-27 18:14:35 +0530

    feat(dsa) : solve deletion of head in linked list

---
+++ LinkedList/deletion_of_head.java
@@ -0,0 +1,78 @@
+// iven the head of a singly linked list, delete the head of the linked list and return the head of the modified list. The head is the first 
+// node of the linked list.
+
+package LinkedList;
+
+import java.util.*;
+
+class Node {
+
+    int data;
+    Node next;
+
+    Node(int data , Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class deletion_of_head {
+
+    private static Node convertToLL(int[] arr) {
+
+        Node head = new Node(arr[0]);
+        Node mover = head;
+
+        for(int i = 1 ; i < arr.length ; i ++) {
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover = temp;
+        }
+
+        return head;
+    }
+
+    private static Node delete_head(Node head) {
+
+        head = head.next;
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of list : ");
+        int size = sc.nextInt();
+
+        int[] list = new int[size];
+
+        System.out.println("Enter elements of list : ");
+        for(int i = 0 ; i < size ; i ++) {
+            list[i] = sc.nextInt();
+        }
+
+        Node head = convertToLL(list);
+
+        Node temp = delete_head(head);
+
+        System.out.println("Traversing linked list after removing head : ");
+
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.print("null");
+
+        System.out.println();
+
+        sc.close();
+    }
+    
+}
